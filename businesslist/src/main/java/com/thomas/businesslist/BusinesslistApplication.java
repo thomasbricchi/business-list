3b6f8a2d0418cfd6810c07fb5c823918faddf345
thomasbricchi <dev@wideside.ch>
2020-12-01 21:47:19 +0100

    be init

---
+++ businesslist/src/main/java/com/thomas/businesslist/BusinesslistApplication.java
@@ -0,0 +1,13 @@
+package com.thomas.businesslist;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class BusinesslistApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(BusinesslistApplication.class, args);
+	}
+
+}
